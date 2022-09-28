b504cea59b3b528a7b8dd25cd94d314f9b613ecc
kuanimike <kuanimike@gmail.com>
2022-09-28 10:16:36 +0800

    Initial Commit

---
+++ Monsters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monsters
+//
+//  Created by kuani Li on 2022/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
